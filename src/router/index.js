2d051be1fcf11c26e8736e20fbc04919a9d842eb
zhchiEarth <zhchi_earth@163.com>
2018-01-12 11:20:47 +0800

    修改登录

--- src/router/index.js
+++ src/router/index.js
@@ -209,49 +209,41 @@ const router = new Router({
 
 export default router
 
-// router.beforeEach((to, from, next) => {
-//     let token = window.localStorage.getItem('token');
-//     console.log(to.matched)
-//     if (to.matched.some(record => record.requiresAuth) && (!token || token === null)) {
-//         console.log(11)
-//         next({
-//             path: '/login',
-//             query: { redirect: to.fullPath }
-//         })
-//     } else {
-//         next()
-//     }
-// })
+router.beforeEach((to, from, next) => {
+    // console.log(1)
+    // let token = window.localStorage.getItem('token');
+    // if ((!token || token === null)) {
+    //     console.log(11)
+    //     next({
+    //         path: '/login',
+    //         query: { redirect: to.fullPath }
+    //     })
+    // } else {
+        next()
+    // }
+})
 
 const whiteList = ['/login'];// 不重定向白名单
 
-router.beforeEach((to, from, next) => {
+// router.beforeEach((to, from, next) => {
+    //
     // if (getToken()) { // 判断是否有token
     //     if (to.path === '/login') {
     //         next({ path: '/' });
     //     } else {
-    next();
-    // if (store.getters.roles.length === 0) { // 判断当前用户是否已拉取完user_info信息
-    //     store.dispatch('GetInfo').then(res => { // 拉取user_info
-    //         const roles = res.data.role;
-    //         store.dispatch('GenerateRoutes', { roles }).then(() => { // 生成可访问的路由表
-    //             router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
-    //             next({ ...to }); // hack方法 确保addRoutes已完成
-    //         })
-    //     }).catch(() => {
-    //         store.dispatch('FedLogOut').then(() => {
-    //             next({ path: '/login' });
-    //         })
-    //     })
-    // } else {
-    //     // 没有动态改变权限的需求可直接next() 删除下方权限判断 ↓
-    //     if (hasPermission(store.getters.roles, to.meta.role)) {
-    //         next();//
-    //     } else {
-    //         next({ path: '/401', query: { noGoBack: true } });
-    //     }
-    //     // 可删 ↑
-    // }
+    //         if (store.getters.roles.length === 0) { // 判断当前用户是否已拉取完user_info信息
+    //             store.dispatch('GetInfo').then(res => { // 拉取user_info
+    //                 const roles = res.data.role;
+    //                 store.dispatch('GenerateRoutes', { roles }).then(() => { // 生成可访问的路由表
+    //                     router.addRoutes(store.getters.addRouters) // 动态添加可访问路由表
+    //                     next({ ...to }); // hack方法 确保addRoutes已完成
+    //                 })
+    //             }).catch(() => {
+    //                 store.dispatch('FedLogOut').then(() => {
+    //                     next({ path: '/login' });
+    //                 })
+    //             })
+    //         }
     //     }
     // } else {
     //     if (whiteList.indexOf(to.path) !== -1) { // 在免登录白名单，直接进入
@@ -260,4 +252,4 @@ router.beforeEach((to, from, next) => {
     //         next('/login'); // 否则全部重定向到登录页
     //     }
     // }
-});
+// })
